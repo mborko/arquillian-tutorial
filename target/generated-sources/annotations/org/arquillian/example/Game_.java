463c004c8353d0c99f403e90feb2f701459cf81d
Michael Borko <michael@borko.at>
2015-06-02 15:25:14 +0200

    Added target directory again

---
+++ target/generated-sources/annotations/org/arquillian/example/Game_.java
@@ -0,0 +1,14 @@
+package org.arquillian.example;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-06-02T15:23:08")
+@StaticMetamodel(Game.class)
+public class Game_ { 
+
+    public static volatile SingularAttribute<Game, Long> id;
+    public static volatile SingularAttribute<Game, String> title;
+
+}
